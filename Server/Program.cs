9a3068c72dc4b66827e5b61da27b618e4874ad87
Tivatamber <tivatamber@gmail.com>
2023-05-31 14:25:57 +0800

    Basic-Server

---
+++ Server/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Server
+{
+    class Program
+    {
+        private const int ConcurrentCount = 20;
+        private const string ServerUrl = "http://localhost:9000/";
+
+        public static void Main(string[] args)
+        {
+            var server = new WebServer(ConcurrentCount);
+            server.Bind(ServerUrl);
+            server.Start();
+            
+            Console.WriteLine($"Web Server started at {ServerUrl}, Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
